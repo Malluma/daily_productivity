bb4d45734dc4f8bc42ec4c70652b3a9497e6086e
Roman V Polonsky <roman@polonsky.tech>
2021-07-23 16:59:36 +0300

    format files, use tabs

--- src/components/SetInterval.js
+++ src/components/SetInterval.js
@@ -1,52 +1,52 @@
 import React from 'react';
 
 function SetInterval(props) {
+	function createNewInterval() {
+		const body = JSON.stringify({
+			value_: 'work',
+			from_: '2021-07-15%10:00:00',
+			to_: '2021-07-15%10:30:00',
+			user_id: '000001',
+		});
 
-  function createNewInterval() {
+		console.log('BODY!!!!');
+		console.log(body);
 
-    const body = JSON.stringify({
-      value_: 'work',
-      from_: '2021-07-15%10:00:00',
-      to_: '2021-07-15%10:30:00',
-      user_id: '000001'
-    })
+		fetch('http://localhost:3001/intervals', {
+			method: 'POST',
+			headers: new Headers({
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			}),
+			body: body,
+		})
+			.then((response) => response.json())
+			.then((json) => console.log(json))
+			.catch((error) => console.error('error', error));
+	}
 
-    console.log('BODY!!!!')
-    console.log(body)
-
-    fetch('http://localhost:3001/intervals',
-      {
-        method: 'POST',
-        headers: new Headers({
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        }),
-        body: body
-      })
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(error => console.error('error', error))
-
-  }
-
-  return (
-    <div className='setInterval'>
-      <div className='title'>Daily Productivity</div>
-      <div className='datetime'>21.08.2020   11:30 - 12:00</div>
-      <button className='btn btnActions work' onClick={createNewInterval}>WORK</button>
-      <button className='btn btnActions mentorship'>MENTORSHIP</button>
-      <button className='btn btnActions study'>STUDY</button>
-      <button className='btn btnActions eat'>EAT</button>
-      <button className='btn btnActions routine'>ROUTINE</button>
-      <button className='btn btnActions idle'>IDLE</button>
-      <button className='btn btnActions games-series'>GAMES, SERIES</button>
-      <button className='btn btnActions hobby'>HOBBY</button>
-      <button className='btn btnActions sport'>SPORT</button>
-      <button className='btn btnActions walk'>WALK</button>
-      <button className='btn btnActions communicate'>COMMUNICATE</button>
-      <button className='btn btnActions sleep'>SLEEP</button>
-    </div>
-  );
+	return (
+		<div className="setInterval">
+			<div className="title">Daily Productivity</div>
+			<div className="datetime">21.08.2020 11:30 - 12:00</div>
+			<button className="btn btnActions work" onClick={createNewInterval}>
+				WORK
+			</button>
+			<button className="btn btnActions mentorship">MENTORSHIP</button>
+			<button className="btn btnActions study">STUDY</button>
+			<button className="btn btnActions eat">EAT</button>
+			<button className="btn btnActions routine">ROUTINE</button>
+			<button className="btn btnActions idle">IDLE</button>
+			<button className="btn btnActions games-series">
+				GAMES, SERIES
+			</button>
+			<button className="btn btnActions hobby">HOBBY</button>
+			<button className="btn btnActions sport">SPORT</button>
+			<button className="btn btnActions walk">WALK</button>
+			<button className="btn btnActions communicate">COMMUNICATE</button>
+			<button className="btn btnActions sleep">SLEEP</button>
+		</div>
+	);
 }
 
 export default SetInterval;
--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -4,101 +4,101 @@ import Simple from './Simple'
 
 const TimeLine = function ({ returnToAppMarkedIntervals }) {
 
-    const [intervalsList, setIntervalsList] = useState([]);
-    const [markedIntervalsTL, setMarkedIntervalsTL] = useState({})
-    const [num_TL, setNum_TL] = useState(555);
+	const [intervalsList, setIntervalsList] = useState([]);
+	const [markedIntervalsTL, setMarkedIntervalsTL] = useState({})
+	const [num_TL, setNum_TL] = useState(555);
 
-    function createEmptyDayLine(isEmpty = true) {
-        const emptyDayLine = [];
+	function createEmptyDayLine(isEmpty = true) {
+		const emptyDayLine = [];
 
-        for (let i = 0; i < 48; i++) {
-            emptyDayLine.push({ 'value': '' });
-        }
+		for (let i = 0; i < 48; i++) {
+			emptyDayLine.push({ 'value': '' });
+		}
 
-        emptyDayLine.push(isEmpty);
+		emptyDayLine.push(isEmpty);
 
-        return emptyDayLine;
-    }
+		return emptyDayLine;
+	}
 
-    function SplitArrByDayLines(arr) {
+	function SplitArrByDayLines(arr) {
 
-        let result = [];
-        let dayLine = [...createEmptyDayLine(false)]
-        let prevDay = '';
+		let result = [];
+		let dayLine = [...createEmptyDayLine(false)]
+		let prevDay = '';
 
-        for (let i = 0; i < arr.length; i++) {
+		for (let i = 0; i < arr.length; i++) {
 
-            const currentDay = arr[i].day
-            console.log(currentDay)
+			const currentDay = arr[i].day
+			console.log(currentDay)
 
-            if (prevDay && currentDay !== prevDay) {
-                result.push([prevDay, ...dayLine]);
-                dayLine = [...createEmptyDayLine(false)]
-            }
+			if (prevDay && currentDay !== prevDay) {
+				result.push([prevDay, ...dayLine]);
+				dayLine = [...createEmptyDayLine(false)]
+			}
 
-            dayLine[arr[i].minutes / 30].value = arr[i].value_;
-            prevDay = currentDay
-        }
+			dayLine[arr[i].minutes / 30].value = arr[i].value_;
+			prevDay = currentDay
+		}
 
-        result.push([prevDay, ...dayLine]);
+		result.push([prevDay, ...dayLine]);
 
-        return result;
-    }
+		return result;
+	}
 
-    function getData() {
-        fetch(
-            'http://localhost:3001/intervals?user_id=000001',
-            { method: 'GET' }
-        )
-            .then(response => response.json())
-            .then(json => setIntervalsList(SplitArrByDayLines(json)))
-            .catch(error => console.error('error', error))
+	function getData() {
+		fetch(
+			'http://localhost:3001/intervals?user_id=000001',
+			{ method: 'GET' }
+		)
+			.then(response => response.json())
+			.then(json => setIntervalsList(SplitArrByDayLines(json)))
+			.catch(error => console.error('error', error))
 
-    }
+	}
 
-    function formatDateToStr_ddmmyy(date) {
-        let curDay = String(date.getDate())
-        let curMonth = String(date.getMonth())
-        let curYear = String(date.getFullYear()).slice(2, 4)
-        return `${(curDay.length === 1) ? '0' + curDay : curDay}.${(curMonth.length === 1) ? '0' + curMonth : curMonth}.${curYear}`
-    }
+	function formatDateToStr_ddmmyy(date) {
+		let curDay = String(date.getDate())
+		let curMonth = String(date.getMonth())
+		let curYear = String(date.getFullYear()).slice(2, 4)
+		return `${(curDay.length === 1) ? '0' + curDay : curDay}.${(curMonth.length === 1) ? '0' + curMonth : curMonth}.${curYear}`
+	}
 
-    Date.prototype.toDateInputValue = (function () {
-        var local = new Date(this);
-        local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
-        return local.toJSON().slice(0, 10);
-    });
+	Date.prototype.toDateInputValue = (function () {
+		var local = new Date(this);
+		local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+		return local.toJSON().slice(0, 10);
+	});
 
-    function addEmptyDay() {
-        setIntervalsList(prev => {
-            return [...prev, [(new Date).toDateInputValue(), ...createEmptyDayLine()]]
-        })
-    }
+	function addEmptyDay() {
+		setIntervalsList(prev => {
+			return [...prev, [(new Date).toDateInputValue(), ...createEmptyDayLine()]]
+		})
+	}
 
-    function returnToTimeLineMarkedIntervals(day, markedIntervalsForDay) {
+	function returnToTimeLineMarkedIntervals(day, markedIntervalsForDay) {
 
-        setMarkedIntervalsTL({ ...markedIntervalsTL, [day]: [...markedIntervalsForDay] })
-        console.log('markedIntervalsTL')
-        console.log(day)
-        console.log(markedIntervalsForDay)
-        console.log(JSON.stringify(markedIntervalsTL))
+		setMarkedIntervalsTL({ ...markedIntervalsTL, [day]: [...markedIntervalsForDay] })
+		console.log('markedIntervalsTL')
+		console.log(day)
+		console.log(markedIntervalsForDay)
+		console.log(JSON.stringify(markedIntervalsTL))
 
-        //returnToAppMarkedIntervals(markedIntervals);
-    }
+		//returnToAppMarkedIntervals(markedIntervals);
+	}
 
-    const returnToTimeLine = useCallback((numFromSimple) => {
-        setNum_TL(numFromSimple)
-    })
+	const returnToTimeLine = useCallback((numFromSimple) => {
+		setNum_TL(numFromSimple)
+	})
 
-    return (<div className='intervals'>
-        <button className='btn getBtn' onClick={getData}>Update</button>
-        <div className='IntervalsTable'>
-            {intervalsList.map((day, j) => <DayLine key={j} lineIndex={j} currentDayArray={[...day]} returnToTimeLineMarkedIntervals={returnToTimeLineMarkedIntervals} returnToAppMarkedIntervals={returnToAppMarkedIntervals} />)}
-        </div>
-        <button className='btn addEmptyDayBtn' onClick={addEmptyDay}>+</button>
-        <Simple returnToTimeLine={returnToTimeLine} />
-        <div className='simpleTL'>{num_TL}</div>
-    </div>)
+	return (<div className='intervals'>
+		<button className='btn getBtn' onClick={getData}>Update</button>
+		<div className='IntervalsTable'>
+			{intervalsList.map((day, j) => <DayLine key={j} lineIndex={j} currentDayArray={[...day]} returnToTimeLineMarkedIntervals={returnToTimeLineMarkedIntervals} returnToAppMarkedIntervals={returnToAppMarkedIntervals} />)}
+		</div>
+		<button className='btn addEmptyDayBtn' onClick={addEmptyDay}>+</button>
+		<Simple returnToTimeLine={returnToTimeLine} />
+		<div className='simpleTL'>{num_TL}</div>
+	</div>)
 }
 
 export default TimeLine
