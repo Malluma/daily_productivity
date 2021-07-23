40a5f2acd2d461da1b097c08f34af36ea4edec7f
Malluma <sudzumia@gmail.com>
2021-07-23 20:39:11 +0700

    format

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import './App.css';
+import '../App.css';
 import TimeLine from './TimeLine';
 import SetIntervals from './SetInterval'
 
---
+++ src/components/DateInDayLine.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+function DateInDayLine({ currentDay, emptyLine }) {
+
+   let [day, setDay] = useState(currentDay)
+
+   return <input className='dateOnTheDaylineInput'
+      type="date"
+      value={day}
+      onChange={(event) => { setDay(event.target.value) }
+      }
+      readOnly={!emptyLine}
+   />
+}
+
+
+export default DateInDayLine
