58c15998dd6471154f3607dd41f4458ea7b4a033
Malluma <sudzumia@gmail.com>
2021-08-07 11:47:27 +0700

    fix bag with not rendering DayLine component with redux

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -16,10 +16,10 @@ const reducer = (state = { intervals: [], markedIntervals: {} }, action) => {
                 }
 
                 let newMarkedIntervals = [];
-                if (markedIntervalsForDay.includes(index)) {
+                if (markedIntervalsForDay.includes(index)) {                   
                     newMarkedIntervals = [...markedIntervalsForDay.filter(k => k !== index)]
                 }
-                else {
+                else {        
                     newMarkedIntervals = [...markedIntervalsForDay, index];
                 }
 
@@ -27,50 +27,36 @@ const reducer = (state = { intervals: [], markedIntervals: {} }, action) => {
             }
         case "CLEAR_MARKED_INTERVALS":
             return { ...state, markedIntervals: {} }
+
         case "ADD_UPDATED_INTERVALS_TO_STATE":
             {
-                
                 const newIntervals = [...state.intervals]
-                // for (let i = 0; i < state.intervals.length; i++){
-                //     for(let j = 0; j < state.intervals[i].length; j++){
-                //     }
-                //     //newIntervals.push()
-                // }
-                
+
                 for (const dayInMarkedIntervals in state.markedIntervals) {
-                    
+
                     let dayArrayInIntervals = []
                     let dayIndexInIntervals = 0;
 
                     for (dayIndexInIntervals = 0; dayIndexInIntervals < state.intervals.length; dayIndexInIntervals++) {
                         if (state.intervals[dayIndexInIntervals][0] === dayInMarkedIntervals) {
-                            //dayArrayInIntervals = state.intervals[dayIndexInIntervals]
                             dayArrayInIntervals = [...state.intervals[dayIndexInIntervals]]
                             break;
                         }
                     }
-
-                    const dayArrayInMarkedIntervals = state.markedIntervals[dayInMarkedIntervals]            
-
-                    for (let i = 0; i < dayArrayInMarkedIntervals.length; i++){
-                        for (let j = 0; j < dayArrayInIntervals.length; j++ ) {
+                    const dayArrayInMarkedIntervals = state.markedIntervals[dayInMarkedIntervals]
+                    for (let i = 0; i < dayArrayInMarkedIntervals.length; i++) {
+                        for (let j = 0; j < dayArrayInIntervals.length; j++) {
                             if (j === dayArrayInMarkedIntervals[i]) {
-                                //dayArrayInIntervals[j].value = action.payload;
-                                dayArrayInIntervals[j] = {value: action.payload};
+                                dayArrayInIntervals[j] = { value: action.payload };
                             }
                         }
                     }
-
                     newIntervals[dayIndexInIntervals] = dayArrayInIntervals
-
                 }
-                console.log('ADD_UPDATED_INTERVALS_TO_STATE')
-                console.log({ ...state, intervals: newIntervals })
-                console.log(state)
-                return { ...state, intervals: newIntervals}
+
+                return { ...state, intervals: newIntervals }
             }
-        default:
-            return state
+        default: return state
     }
 }
 
